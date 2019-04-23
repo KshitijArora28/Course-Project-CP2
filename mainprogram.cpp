72948c76046194f9e20ebad2195267e1cd08b153
Kshitij Arora <kshitijaroraa@gmail.com>
2019-04-23 20:46:02 +0800

    k

--- mainprogram.cpp
+++ mainprogram.cpp
@@ -20,9 +20,11 @@ int main(){
   cout<<"O->Sort items alpahbetically"<<endl;
   cout<<"T->Terminate code"<<endl;
 
+  cin>>choice;
   int or_size=0;
   int new_size=0;
   product * p[]=new product[0];
+  while(choice!='T'){
   switch(choice){
     //switch (/* expression */) {
       case 'I':
@@ -31,6 +33,28 @@ int main(){
       cin>>n;
       new_size=or_size+n;
       insert_item(p,or_size,new_size);
+      break;
+
+      case 'C':
+      cout<<"Enter product name to change info: -"<<endl;
+      string nm;
+      cin>>nm;
+      cout<<"Enter new quantity"<<endl;
+      cout<<"If new and old quantity are same enter 0"<<endl;
+      int q;
+      cin>>q;
+      cout<<"Enter new cost"<<endl;
+      cout<<"If new and old cost are same enter 0"<<endl;
+      int c;
+      cin>>c;
+      cout<<"Enter new expiry date"<<endl;
+      cout<<"If new and old expiry date are same enter N"<<endl;
+      string e;
+      cin>>e;
+      change(p,nm,q,c,e,or_size);
+      break;
+
     }
+  }
   }
 }
