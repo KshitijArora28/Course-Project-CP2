ccc637040d56fe636b329aa4282526783211b5be
Kshitij Arora <kshitijaroraa@gmail.com>
2019-04-23 12:51:55 +0800

    Update mainprogram.cpp

--- mainprogram.cpp
+++ mainprogram.cpp
@@ -8,4 +8,21 @@ struct product{
   char expiry_date[8];
   double cost;
 };
-int main()
+int main(){
+  char choice;
+  cout<<"Enter choice"<<endl;
+  cout<<"D->Display inventory"<<endl;
+  cout<<"I->Insert new commodities"<<endl;
+  cout<<"C->Change commodity info"<<endl;
+  cout<<"R->Remove commodity"<<endl;
+  cout<<"E->Economic advise based on stock data"<<endl;
+  cout<<"S->Search item"<<endl;
+  cout<<"O->Sort items alpahbetically"<<endl;
+  cout<<"T->Terminate code"<<endl;
+
+  switch(choice){
+    //switch (/* expression */) {
+      case /* value */:
+    }
+  }
+}
