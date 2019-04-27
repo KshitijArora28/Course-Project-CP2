4bb5c60a17ec39503074f71491ecdb6272478bed
Kshitij Arora <kshitijaroraa@gmail.com>
2019-04-27 20:34:14 +0800

    k

--- Display.cpp
+++ Display.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 void display(product * &p1,int n1){
@@ -12,10 +13,20 @@ void display(product * &p1,int n1){
       maxlenpos=i;
     }
   }
+
+  ofstream fout;
+  fout.open("inventory_details.txt");
+  if(fout.fail()){
+    cout<<"Error in file opening !"<<endl;
+    exit(1);
+  }
+
   w=p1[maxlenpos].name.length();
+  fout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<endl;
   cout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<endl;
   cout<<endl;
   for(int j=1;j<n1;j++){
+    fout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(10)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<endl;
     cout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(10)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<endl;
   }
 }
