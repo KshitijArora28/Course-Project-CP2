c3263f8ec706d3993a364eadd9f57f3b736e29af
Kshitij Arora <kshitijaroraa@gmail.com>
2019-04-27 20:59:29 +0800

    k

--- Display.cpp
+++ Display.cpp
@@ -22,12 +22,12 @@ void display(product * &p1,int n1){
   }
 
   w=p1[maxlenpos].name.length();
-  fout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<endl;
-  cout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<endl;
+  fout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<setw(10)<<"Expiry date"<<endl;
+  cout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<setw(10)<<"Expiry date"<<endl;
   cout<<endl;
   for(int j=1;j<n1;j++){
-    fout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(10)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<endl;
-    cout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(10)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<endl;
+    fout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(10)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<setw(10)<<p1[j].e_dd<<" "<<p1[j].e_mm<<" "<<p1[j].e_year<<endl;
+    cout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(10)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<setw(10)<<p1[j].e_dd<<" "<<p1[j].e_mm<<" "<<p1[j].e_year<<endl;
   }
   fout.close();
 }
