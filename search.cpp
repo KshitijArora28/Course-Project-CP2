360fbfaee2c258f5b516771d76dbb537c1b853bf
vyshku18 <47135656+vyshku18@users.noreply.github.com>
2019-04-27 21:05:11 +0800

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -5,12 +5,12 @@ using namespace std;
 void search(int n,product* prod){
   string *search_list=new string[n];
   char key;
-  cout>>"Search by:-">>endl;
-  cout>>"a. Name (case sensitive)">>endl;
-  cout>>"b. Product Code">>endl;
-  cout>>"c. Inventory Status(In-stock products)"
-  cout>>"d. Inventory Status(Out-of-stock products)"
-  cout>>"Enter a,b,c or d: ";
+  cout<<"Search by:-"<<endl;
+  cout<<"a. Name (case sensitive)"<<endl;
+  cout<<"b. Product Code"<<endl;
+  cout<<"c. Inventory Status(All In-stock products)"<<endl;
+  cout<<"d. Inventory Status(All Out-of-stock products)"<<endl;
+  cout<<"Enter a,b,c or d: ";
   cin>>key;
   if (key=='a'){
     cout<<"Searching under names. Enter name: "<<endl;
@@ -22,7 +22,7 @@ void search(int n,product* prod){
     cout<<"Searching under in-stock"<<endl;
   }
   else if(key=='d'){
-    cout<<"Searching under in-stock"<<endl;
+    cout<<"Searching under out-of-stock"<<endl;
   }
   string search_item;
   int list_position=0;
@@ -30,16 +30,16 @@ void search(int n,product* prod){
       cin>>search_item;
       for (int i=1;i<n;i++){
         if(prod[i].name.find(search_item)!=string::npos){
-          search_list[list_position]=prod[i];
+          search_list[list_position]=prod[i].name;
           list_position++;
         }
       }
     }
   else if(key=='b'){
-    cin<<search_item;
+    cin>>search_item;
     for (int i=1;i<n;i++){
       if(prod[i].code.find(search_item)!=string::npos){
-        search_list[list_position]=prod[i];
+        search_list[list_position]=prod[i].code;
         list_position++;
       }
     }
@@ -47,7 +47,7 @@ void search(int n,product* prod){
   else if(key=='c'){
     for (int i=1;i<n;i++){
       if(prod[i].stock=='i'){
-        search_list[list_position]=prod[i];
+        search_list[list_position]=prod[i].name;
         list_position++;
       }
     }
@@ -55,7 +55,7 @@ void search(int n,product* prod){
   else if(key=='d'){
     for (int i=1;i<n;i++){
       if(prod[i].stock=='o'){
-        search_list[list_position]=prod[i];
+        search_list[list_position]=prod[i].name;
         list_position++;
       }
     }
@@ -65,11 +65,11 @@ void search(int n,product* prod){
     return;
   }
   if (list_position==0){
-    cout<<"No valid search result."
+    cout<<"No valid search result."<<endl;
   }
   else{
     cout<<"Search Results are: "<<endl;
-    for (int i=1;i<list_position;i++){
+    for (int i=0;i<list_position;i++){
       cout<<search_list[i]<<endl;
     }
   }
