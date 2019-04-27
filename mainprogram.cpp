506646eb63a564ec443aa52d2bf654feeace4828
KshitijArora28 <47174028+KshitijArora28@users.noreply.github.com>
2019-04-27 15:53:21 +0800

    Create mainprogram.cpp

---
+++ mainprogram.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+
+struct product{
+  string code;
+  string name;//given by user
+  int quantity;//given by user
+  int number_sold;
+  int e_dd,e_mm,e_year;//expiry date given by user
+  char stock;
+  double cost;//given by user
+  double selling_cost;//gven by user
+};
+
+int main(){
+  char choice;
+  cout<<"Choices for Operation"<<endl;
+  cout<<"D->Display inventory"<<endl;
+  cout<<"I->Insert new commodities"<<endl;
+  cout<<"C->Change commodity info"<<endl;
+  cout<<"R->Remove commodity"<<endl;
+  cout<<"E->Economic advise based on stock data"<<endl;
+  cout<<"S->Search item"<<endl;
+  cout<<"O->Sort items alpahbetically"<<endl;
+  cout<<"Q->Sell and check stock"<<endl;
+  cout<<"T->Terminate code"<<endl;  cin>>choice;
+  int or_size=1;
+  int new_size;
+  product * p[]=new product[1];
+
+  while(choice!='T'){
+  switch(choice){
+      case 'I':
+      int n;
+      cout<<"Enter the number of commodities to be appended: -"<<endl;
+      cin>>n;
+      new_size=or_size+n;
+      insert_item(p,or_size,new_size);
+      break;
+
+      case 'C':
+      cout<<"Enter product name to change info (case-sensitive): -"<<endl;
+      string nm;
+      cin>>nm;
+      cout<<"Enter new quantity"<<endl;
+      cout<<"If new and old quantity are same enter 0"<<endl;
+      int q;
+      cin>>q;
+      cout<<"Enter new cost"<<endl;
+      cout<<"If new and old cost are same enter 0"<<endl;
+      int c;
+      cin>>c;
+      cout<<"Enter new expiry date"<<endl;
+      cout<<"If new and old expiry date are same enter N"<<endl;
+      string e;
+      cin>>e;
+      change(p,nm,q,c,e,or_size);
+      break;
+
+      case 'R':
+      cout<<cout<<"Enter product name to delete: -"<<endl;
+      string s;
+      cin>>n;
+      delete_commodity(p,or_size,s);
+      break;
+
+      case 'Q':
+      cout<<"Enter product name to check stock: -"<<endl;
+      string f;
+      cin>>f;
+      stock(p,f,or_size);
+      break;
+
+    }
+  cout<<"Enter next choice:"<<endl;
+  cout<<"D->Display inventory"<<endl;
+  cout<<"I->Insert new commodities"<<endl;
+  cout<<"C->Change commodity info"<<endl;
+  cout<<"R->Remove commodity"<<endl;
+  cout<<"E->Economic advise based on stock data"<<endl;
+  cout<<"S->Search item"<<endl;
+  cout<<"O->Sort items alpahbetically"<<endl;
+  cout<<"Q->Sell and check stock"<<endl;
+  cout<<"T->Terminate code"<<endl;
+  cin>>choice;
+  }
+  return 0;
+}
