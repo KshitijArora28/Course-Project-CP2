4a852cfb5f6f27166db43f45824ca08cf9897a71
Kshitij Arora <kshitijaroraa@gmail.com>
2019-04-27 23:24:39 +0800

    k

--- Emergency_project.cpp
+++ Emergency_project.cpp
@@ -179,7 +179,7 @@ void alerts(product * &p,int n){
   }
 }
 
-void economic(int n,product *p){
+void economic(int n,product * &p){
   int time_period;
   cout<<"Enter the time period (in number of days) for which the data has been collected: ";
   cin>>time_period;
@@ -363,8 +363,8 @@ void search(int n,product* prod){
       cout<<setw(w+4)<<"Name"<<setw(10)<<"Code"<<setw(10)<<"Stock"<<setw(10)<<"Cost"<<setw(14)<<"Sellprice"<<setw(10)<<"Sales"<<setw(14)<<"Expiry date"<<endl;
       cout<<endl;
       for(int j=1;j<n1;j++){
-        fout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(14)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<setw(14)<<p1[j].e_dd<<" "<<p1[j].e_mm<<" "<<p1[j].e_year<<endl;
-        cout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(14)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<setw(14)<<p1[j].e_dd<<" "<<p1[j].e_mm<<" "<<p1[j].e_year<<endl;
+        fout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(14)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<setw(10)<<p1[j].e_dd<<" "<<p1[j].e_mm<<" "<<p1[j].e_year<<endl;
+        cout<<setw(w+4)<<p1[j].name<<setw(10)<<p1[j].code<<setw(10)<<p1[j].quantity<<setw(10)<<p1[j].cost<<setw(14)<<p1[j].selling_cost<<setw(10)<<p1[j].number_sold<<setw(10)<<p1[j].e_dd<<" "<<p1[j].e_mm<<" "<<p1[j].e_year<<endl;
       }
       fout.close();
     }
