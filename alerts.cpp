eb2f788f8818c645693e71d46ac4ca1d5ce333a6
Kshitij Arora <kshitijaroraa@gmail.com>
2019-04-27 19:56:15 +0800

    k

--- alerts.cpp
+++ alerts.cpp
@@ -3,33 +3,38 @@
 //auto order
 #include <ctime>
 
-void alerts(product *p,int n){
+void alerts(product * &p,int n){
   cout<<endl<<"**ALERTS**"<<endl;
   for (int i=0;i<n;i++){
     if (p[i].quantity<5){
-      if (p[i].quantity==0){
+      if (p[i].stock='O'){
           cout<<p[i].name<<" is out-of-stock."<<endl;
+          cout<<endl;
       }
       else{
         cout<<p[i].name<<" stock is low.(less than 5 units remaining)"<<endl;
+        cout<<endl;
       }
     }
   }
   time_t now = time(0);
   tm *gmtm = gmtime(&now);
   for (int i=0;i<n;i++){
-    if (p[i].e_year<=tm_year-1900 && p[i].e_year!=0){
-      if (p[i].e_year<tm_year-1900){
+    if (p[i].e_year-1900<=tm_year && p[i].e_year!=0){
+      if (p[i].e_year-1900<tm_year){
         cout<<p[i].name<<" expiry date is up."<<endl;
+        cout<<endl;
         continue;
       }
-      if(p[i].e_mm<=tm_mon-1){
-        if(p[i].e_mm<tm_mon-1){
+      if(p[i].e_mm-1<=tm_mon){
+        if(p[i].e_mm-1<tm_mon){
           cout<<p[i].name<<" expiry date is up."<<endl;
+          cout<<endl;
           continue
         }
         if(p[i].dd<tm_mday){
           cout<<p[i].name<<" expiry date is up."<<endl;
+          cout<<endl;
         }
       }
     }
--- stock.cpp
+++ stock.cpp
@@ -11,7 +11,7 @@ void stock(product * &p1,string f1,int s1){
         p1[i].quantity=p1[i].quantity-p1[i].number_sold;
       }
       if(p1[i].number_sold>p1[i].quantity){
-        cout<<"Purchase quantity "<<p1[i].number_sold<<" more than stock !"<<endl;
+        cout<<"Purchase quantity is "<<p1[i].number_sold<<" which is more than stock !"<<endl;
         cout<<"Stock left is: - "<<p1[i].quantity<<endl;
         cout<<"Do you want to buy ?"<<endl;
         cout<<"Enter Y for yes and N for no: -"<<endl;
@@ -27,7 +27,8 @@ void stock(product * &p1,string f1,int s1){
       else{
         p1[i].stock='O';
       }
+      cout<<"Stock left is "<<p1[i].quantity<<endl;
+      cout<<endl;
     }
-    cout<<"Stock left is "<<p1[i].quantity<<endl;
   }
 }
